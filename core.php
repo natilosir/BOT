04f802ebb89885029a24b6230e1b86fb2e95a8e8
MMD <natilosnts@gmail.com>
2024-11-02 00:42:26 +0330

    Add files via upload

---
+++ core.php
@@ -0,0 +1,28 @@
+<?php
+error_reporting(E_ALL); // Report all PHP errors
+ini_set('display_errors', 1); // Display errors on screen
+
+//require 'ORM/ORM.php';
+require 'http.php';
+$BOT_TOKEN = '6877437458:AAE4VKHXOYegB7e4ylsbMfmzqprICompWWU';
+define('api', 'https://api.telegram.org/bot'.$BOT_TOKEN.'/');
+
+class bot
+{
+    public static function sendMessage($chat_id, $text, $reply_to_message_id = null)
+    {
+        $data_received = [
+            'chat_id' => $chat_id,
+            'text' => $text,
+            'reply_to_message_id' => $reply_to_message_id,
+        ];
+
+
+        return http(api.'sendMessage', $data_received );
+
+    }
+}
+
+print_r(
+    bot::sendMessage('5823183588','می گم بیا پایین')
+);
