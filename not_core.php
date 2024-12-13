ae2d7d297412aaad43ab6ba8ba83cf7902222490
MMD <natilosnts@gmail.com>
2024-12-13 19:50:18 +0330

    Delete not_core.php

--- not_core.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Enable error logging in the current directory for PHP 5.6
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', '1');
-ini_set('log_errors', '1');
-ini_set('error_log', __DIR__ . '/error.log');
